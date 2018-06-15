545f9f8c7a68ea2db6d56e1ebcf8453f5b1679af
vsubhuman <vsubhuman@yandex-team.ru>
2018-06-16 00:33:44 +0300

    Added browser locale detection

--- cardano-calculator/assets/js/cardano-calculator.js
+++ cardano-calculator/assets/js/cardano-calculator.js
@@ -1,3 +1,16 @@
+function browserLocale () {
+    if (navigator.languages && navigator.languages.length) {
+        // latest versions of Chrome and Firefox set this correctly
+        return navigator.languages[0]
+    } else if (navigator.userLanguage) {
+        // IE only
+        return navigator.userLanguage
+    } else {
+        // latest versions of Chrome, Firefox, and Safari set this correctly
+        return navigator.language
+    }
+}
+
 function escapeRegExp(str) {
     // noinspection RegExpRedundantEscape
     return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
@@ -345,7 +358,7 @@ function initCalcLayout(layoutName = Cookies.get('layout')) {
 }
 
 function initLocale() {
-    let locale = 'en-US';
+    let locale = browserLocale() || 'en-US';
     window.CardanoCalculatorLocale = Object.freeze({
         locale: locale,
         separators: getSeparatorsByLocale(locale)
