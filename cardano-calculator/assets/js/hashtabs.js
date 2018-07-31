78b6b83799409413fc1a7876154b835dd60e65f2
vsubhuman <vsubhuman@yandex-team.ru>
2018-07-31 15:14:41 +0300

    Unified navbars with the main calculator

--- cardano-calculator/assets/js/hashtabs.js
+++ cardano-calculator/assets/js/hashtabs.js
@@ -2,20 +2,24 @@ function __HashTabsConstructor() {
 
     let _tabsSelector = '.hashtab';
     let _linksSelector = '.hashlink';
+    let _scrollToTop = true;
 
     function selectTab(url) {
         let target = url ? url.split('#') : null;
         if (target[1] && target[1] !== this.tab) {
             $(_tabsSelector).filter('[href="#' + target[1] + '"]').tab('show');
-            window.scrollTo(0, 0);
+            if (_scrollToTop) {
+                window.scrollTo(0, 0);
+            }
             history.pushState("", document.title, url);
             this.tab = target[1]
         }
     }
 
-    this.bind = function ({tabsSelector = _tabsSelector, linksSelector = _linksSelector, init = true} = {}) {
+    this.bind = function ({tabsSelector = _tabsSelector, linksSelector = _linksSelector, init = true, scrollToTop = _scrollToTop} = {}) {
         _tabsSelector = tabsSelector;
         _linksSelector = linksSelector;
+        _scrollToTop = scrollToTop;
         $(document).on('click', tabsSelector + ', ' + linksSelector, function(e) {
             e.preventDefault();
             selectTab(this.href);
